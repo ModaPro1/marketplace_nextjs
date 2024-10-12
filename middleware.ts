f626bf470c8691aeb6777b7f171fd410e8dbdbc8
Mahmoud <mahmoudfarghly519@gmail.com>
2024-10-12 12:59:19 +0300

    edit middleware

--- middleware.ts
+++ middleware.ts
@@ -37,6 +37,6 @@ export async function middleware(request: NextRequest) {
 
   return NextResponse.next();
 }
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+// };
