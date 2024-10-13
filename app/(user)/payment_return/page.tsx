438f6f6e9539319ca88f33f5141625f4973e4a15
Mahmoud <mahmoudfarghly519@gmail.com>
2024-10-13 15:48:03 +0300

    uncommented payment_return

--- app/(user)/payment_return/page.tsx
+++ app/(user)/payment_return/page.tsx
@@ -5,21 +5,21 @@ import { FaCircleXmark } from "react-icons/fa6";
 
 export default async function PaymentReturnPage({ searchParams }: { searchParams: any }) {
   const sessionId = searchParams.session_id;
-  // if (!sessionId) {
-  //   notFound();
-  // }
-  // const result = await fetch(`${process.env.SITE_URL}/api/checkout_session?session_id=${sessionId}`, {cache: "no-store"});
+  if (!sessionId) {
+    notFound();
+  }
+  const result = await fetch(`${process.env.SITE_URL}/api/checkout_session?session_id=${sessionId}`, {cache: "no-store"});
 
-  // const data = await result.json();
+  const data = await result.json();
   
-  // if(data.error) {
-  //   notFound()
-  // }
+  if(data.error) {
+    notFound()
+  }
 
   return (
     <>
       <div className="text-center pt-40 min-h-[100vh] bg-[#f4f5f7]">
-        {/* {data.status == "complete" ? (
+        {data.status == "complete" ? (
           <div className="mx-auto box bg-white p-5 rounded-md shadow w-full sm:w-96 border-b-4 border-[#28a745]">
             <FaCheckCircle fontSize={55} className="text-[#28a745] inline" />
             <h2 className="my-4 text-3xl font-medium">Your payment was successful</h2>
@@ -44,7 +44,7 @@ export default async function PaymentReturnPage({ searchParams }: { searchParams
           <Link href="/contact" className="underline">
             Contact us
           </Link>
-        </p> */}
+        </p>
       </div>
     </>
   );
