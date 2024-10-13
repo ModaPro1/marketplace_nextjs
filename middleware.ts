63a4892d5054195814ae7a7aeee9dd6c15cab495
Mahmoud <mahmoudfarghly519@gmail.com>
2024-10-13 15:39:57 +0300

    vercel re deploy

--- middleware.ts
+++ middleware.ts
@@ -38,5 +38,5 @@ export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
 };
