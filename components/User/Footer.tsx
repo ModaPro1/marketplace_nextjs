87887f2217b441390b7e4974781f36f08e9f3c42
Mahmoud <mahmoudfarghly519@gmail.com>
2024-10-20 18:48:39 +0300

    update personal portfolio link

--- components/User/Footer.tsx
+++ components/User/Footer.tsx
@@ -1,11 +1,10 @@
-import Link from "next/link";
 import { FaHeart } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div className="bg-[#191919] text-[#b9b9b9] p-4 flex items-center justify-center gap-1.5">
       Made With <FaHeart className="text-red-800" /> By{" "}
-      <a href="https://mahmoud-ahmed-portofolio.vercel.app/" target="_blank" className="text-main">
+      <a href="https://mahmoud-ahmed.vercel.app/" target="_blank" className="text-main">
         Mahmoud Ahmed
       </a>
     </div>
